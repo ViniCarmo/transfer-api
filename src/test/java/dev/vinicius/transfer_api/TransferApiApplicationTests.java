7402437b9172d8fd8afef57c9b1a601693676315
ViniCarmo <vinicius.carmo1106@gmail.com>
2026-04-01 14:01:54 -0300

    Create project

---
+++ src/test/java/dev/vinicius/transfer_api/TransferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vinicius.transfer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
